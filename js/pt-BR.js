45cb264cb4c1e4e6fb1d53a1c0fed71037316a9f
Debora <deborasousasilva70@gmail.com>
2025-07-06 13:30:24 -0300

    convite

---
+++ js/pt-BR.js
@@ -0,0 +1,22 @@
+var lang_nombreRequerido = 'Insira o nome';
+var lang_informandoAsistencia = 'Relatando presença';
+var lang_confirmarAsistencia = 'Enviar';
+
+var lang_caracteresNombreAsistencia = 'O nome não deve ter mais de 20 caracteres';
+var lang_caracteresComentariosAsistencia = 'A mensagem não deve exceder 100 caracteres';
+
+var lang_asistenciaMsjExito_1 = 'Informação enviada';
+var lang_asistenciaMsjExito_2 = 'Enviamos suas informações de comparecimento';
+
+var lang_titleModalAsistenciaCeremonia = 'Você irá à cerimônia?';
+var lang_titleModalAsistenciaFiesta = 'Você irá à festa?';
+
+var lang_titleModalMapaFiesta = 'Como chegar à festa';
+var lang_titleModalMapaCeremonia = 'Como chegar à Cerimônia';
+
+
+var lang_codigoAccesoVipRequerido = 'Digite o código de acesso';
+var lang_btnCodigoVip = 'Acessar';
+var lang_accediendoCodigoVip = 'Acessando';
+
+var lang_textoFinalCuentaRegresiva = 'O GRANDE <br> DIA CHEGOU!';
